c6ac31702bf10de35a6ff1b6d0946d199864f3ce
dc-ops <darshan.chaluvaraju.github@gmail.com>
2024-11-03 17:12:27 +0530

    puppy is born

---
+++ go.mod
@@ -0,0 +1,5 @@
+module dogs
+
+go 1.23.2
+
+require github.com/DC-Ops/puppy v0.0.0-20241103104824-5fb03055eb33 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DC-Ops/puppy"
+)
+
+func main() {
+	fmt.Println("Hello Go!")
+	s1 := puppy.Bark()
+	fmt.Println(s1)
+}
